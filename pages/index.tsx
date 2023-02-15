6f3dce9d4f421a25b388df3e6379bb08b6826a1e
prabhathk <prabhath14411@gmail.com>
2023-02-15 21:52:53 +0530

    Added two images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,7 +7,24 @@ export default function Home() {
       <Head>
         <title>Office Lite</title>
       </Head>
-      <main></main>
+      <main>
+        <div className="relative">
+          <Image
+            className="absolute -mr-[42px] -mt-[170px]"
+            src="/assets/home/bg-pattern-header.svg"
+            alt="Logo"
+            width={458}
+            height={458}
+          ></Image>
+          <Image
+            className="absolute  inset-0 pt-[46px]"
+            src="/assets/shared/logo.svg"
+            alt="Logo"
+            width={155}
+            height={32}
+          ></Image>
+        </div>
+      </main>
     </>
   );
 }
